9c8b9a8c61e0739ece17d3bd9f91049c9579828a
Birkelbach Diana <diana.birkelbach@orbis.de>
2020-10-15 16:39:37 +0200

    using submit mode never

--- OpenFormAsADialog/formScriptForTheDialog.js
+++ OpenFormAsADialog/formScriptForTheDialog.js
@@ -8,54 +8,26 @@ function Dianamics_Onload(executionContext){
     formContext.ui.headerSection.setCommandBarVisible(false);
     formContext.ui.headerSection.setTabNavigatorVisible(false);
 
-    const myAttributeNames = ["orb_comment", "orb_count", "orb_optionsetcode"];
-    const myAttributes = myAttributeNames.map((attrName) => [attrName, formContext.getAttribute(attrName)]);
-   
-    const clearAction = () => {
-        const actionButton = formContext.getAttribute("orb_action");
-        if(actionButton.getValue()==="Evaluate"){
-            actionButton.setValue(null);
-        }
-    }
-    
-    const getMyData = () => {               
-        const myValues = myAttributes.map(([attrName, attribute]) =>{ 
-            const val = attribute.getValue(attrName);
-                 //clear the values, this way the form won't be dirty afterwards
-            attribute.setValue(null);
-            return [attrName, val] 
-        });
-        const isDirty = myValues.some(([attrName, value]) => value!=null);        
-        return {
-            values : myValues,
-            isDirty : isDirty
-        };
-    }
-   
-  
+    //set all attributes in "never" submit
+    formContext.data.entity.attributes.forEach((attribute) =>   attribute.setSubmitMode  && attribute.setSubmitMode("never"))
 
-    const myAction = function(context, data){
-        const dataToSave = data !=null ? data : getMyData();
-        if(dataToSave.isDirty===true){      
-            clearAction();                        
-            console.log(data);  
-            Xrm.Navigation.openAlertDialog({text: "Here I can make whatever I need"}).then(() => formContext.ui.close());
+
+    const myAction = function(){
+        const myAttributeNames = ["orb_comment", "orb_count", "orb_optionsetcode"];
+        const dataToSave = myAttributeNames.map((attributeName) => {
+            const attribute = formContext.getAttribute(attributeName);
+            return [attributeName, attribute.getValue()];
+        })
+        const isDirty = dataToSave.some(([attrName, value]) => value!=null)
+        if(isDirty===true){            
+            Xrm.Navigation.openAlertDialog({text: `Here I can make whatever I need with: ${JSON.stringify(dataToSave)}`})
+                .then(() => formContext.ui.close());
         }
         else{
             formContext.ui.close();        
         }
     }
 
-    const onSave = function(saveContext){                              
-        const myData = getMyData();
-        if(myData.isDirty===true){
-            saveContext.getEventArgs().preventDefault();          
-            myAction(null, myData);                   
-        }
-        
-    }
-
-    formContext.data.entity.addOnSave(onSave);
     formContext.getAttribute("orb_action").addOnChange(myAction);
 
 }
--- OpenFormAsADialog/open.js
+++ OpenFormAsADialog/open.js
@@ -9,6 +9,7 @@ Xrm.Navigation.navigateTo(
             id: Xrm.Page.data.entity.getId(), 
             name: "dummy"
         },
+        entityId: "83BD09D6-930D-EB11-A813-000D3A23CB53",  //specify this id if you have defined a special Dialog-Record in your entity, because of the naming issue
         formId: "830fb574-f340-40e6-ad64-93b2033a1c6e" //the form customized for the dialog
     }, {
         target: 2, 
